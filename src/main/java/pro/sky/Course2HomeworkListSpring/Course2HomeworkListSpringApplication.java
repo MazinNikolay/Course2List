cef05e8c7583e5312068b204e905fa8d05e1cbc3
Nikolay <koliamba_5@mail.ru>
2024-02-02 07:06:36 +0800

    Initial

---
+++ src/main/java/pro/sky/Course2HomeworkListSpring/Course2HomeworkListSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2HomeworkListSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2HomeworkListSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2HomeworkListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Course2HomeworkListSpring/Course2HomeworkListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2HomeworkListSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2HomeworkListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
